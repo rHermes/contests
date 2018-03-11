b6538de855d826b491540fa1a8039be31181e965
rHermes <teodor_spaeren@riseup.net>
2018-03-11 14:01:38 +0100

    Fix solutions for 1001 and 1002
    The 1001 gave me more pause than expected, but that was due to not
    realizing that my computer is 64 bit and the judge is 32 bit. The scanf
    string was the culprit. The solution to 1002 was easier once I realized
    how to do it, but it was not as easy as it first seemed.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Contests
+
+These are various programming contests that I've solved.
+
+
+## Status
+
+### Timus
+http://acm.timus.ru/author.aspx?id=246961
+
+- 1000 | A+B Problem | Accepted | 0.001 | 204 KB
+- 1001 | Reverse Root | Accepted | 0.124 | 1248 KB
+- 1002 | Phone Numbers | Accepted | 0.031 | 752 KB
+- 1006 | Square Frames | Accepted | 0.015 | 212 KB
--- timus/p1001.c
+++ timus/p1001.c
@@ -5,25 +5,32 @@
 #include <stdint.h>
 #include <math.h>
 
-#define BUFFSIZE 2000
+
+// We need this, because my machine has a different
+// style than the online judge, which is 32bit.
+#ifdef ONLINE_JUDGE
+#define LONG_SCAN "%llu"
+#else
+#define LONG_SCAN "%lu"
+#endif
 
 int main() {
-	uint32_t buff[BUFFSIZE];
+	double buff[128*1024];
 	int32_t N = 0;
 	
 	int rc;
-	uint32_t tmp;
-	rc = scanf("%u", &tmp);
+	uint64_t tmp;
+	rc = scanf(LONG_SCAN, &tmp);
 	while (rc != EOF) {
-		buff[N] = tmp;
+		buff[N] = sqrt(tmp * 1.0);
 		N++;
-		rc = scanf("%u", &tmp);
+		rc = scanf(LONG_SCAN, &tmp);
 	}
 
 	// We are now done.
 	for (int32_t i = N-1; i >= 0; i--) {
-		float f = sqrtf(buff[i]);
-		printf("%.6f\n", f);
+		printf("%.4lf\n", buff[i]);
 	}
+
 	return 0;
 }
