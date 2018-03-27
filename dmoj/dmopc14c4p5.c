b4d73f8fe4ba15fcf6a9bc3938ffd651eab222f4
rHermes <teodor_spaeren@riseup.net>
2018-03-27 20:35:27 +0200

    Start on dmopc14c4p5

---
+++ dmoj/dmopc14c4p5.c
@@ -0,0 +1,28 @@
+/*
+ * dmopc14c4p5 - DMOPC '14 Contest 5 P5 - Aircraft Carrier Akagi
+ * 
+ * This is an interesting challenge.
+ *
+ * The target k, must be odd, and you can get j from k, by doing
+ *
+ * j = (k+1)/2
+ *
+ *
+ * I want to solve this with a bruteforce first, and then after that
+ * we can try to get clever.
+ *
+ * I think it is possible, that we never move one of the points.
+ * I do not know this for sure, but if it is so, then we can try
+ * to use this as a baseline for generating all possible solutions
+ * and test. It might even be fast enough to generate all solutions.
+ *
+ * There might also be a solution to this involving recursion in a 
+ * way. I do not yet know. Maybe partial sums?
+ *
+ */
+
+#include <stdio.h>
+
+int main() {
+	return 0;
+}
