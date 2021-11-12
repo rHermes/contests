8675dec509289d8a4f55f2f76a490bc819992312
Teodor Spæren <teodor@sparen.no>
2021-11-12 15:54:14 +0100

    Change input file pattern and add outputs

--- aoc/src/common/input.hpp
+++ aoc/src/common/input.hpp
@@ -14,7 +14,7 @@ namespace aoc {
 
     // TODO(rHermes): Improve this system.
     std::optional<std::string> get_input(int year, int day) {
-        std::string name = fmt::format("aoc/inputs/y{:04d}_d{:02d}.input", year, day);
+        std::string name = fmt::format("aoc/inputs/{:04d}-{:02d}.input", year, day);
         if (!fs::exists(name)) {
             return {};
         }
