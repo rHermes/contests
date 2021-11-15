3c2e3c5306ec3ce1cce8ae711c671110e10cf70d
Teodor Spæren <teodor@sparen.no>
2021-11-15 11:38:21 +0100

    Speed up 2015 Day 3
    This converts 2015 Day 3 to use a bitset instead of an set. This speeds
    thigns up considerably, but to get it to work, I've had to make some
    assumptions about the inputs. I'm not sure if I like this, but I'll keep
    it as it is. The goal for now is to write the fastest solution that will
    work on all aoc server outputs, without being to cheeky about it.
    
    This change also updates the version of the fmt library to the latest
    main. There has been some speedups since the last version and we want
    that.

--- aoc/src/years/2015/day03.cpp
+++ aoc/src/years/2015/day03.cpp
@@ -1,70 +1,61 @@
 #include "days.hpp"
 
+#include <algorithm>
+#include <bitset>
+
 #include <fmt/core.h>
 #include <fmt/format.h>
 
-#include <tuple>
-#include <unordered_set>
-#include <utility>
 
 using aoc::In;
 using aoc::Out;
 
-template <class T> inline void hash_combine(std::size_t &seed, const T &v) {
-  std::hash<T> hasher;
-  seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-}
-
-namespace std {
-template <typename S, typename T> struct hash<pair<S, T>> {
-  inline size_t operator()(const pair<S, T> &v) const {
-    size_t seed = 0;
-    ::hash_combine(seed, v.first);
-    ::hash_combine(seed, v.second);
-    return seed;
-  }
-};
-} // namespace std
-
-using Point = std::pair<int, int>;
-
-static const Point perform_move(Point pos, char c) {
-  const auto [x, y] = pos;
-  switch (c) {
-  case '>':
-    return {x + 1, y};
-  case '^':
-    return {x, y + 1};
-  case '<':
-    return {x - 1, y};
-  case 'v':
-    return {x, y - 1};
-  default:
-    return {x, y};
-  }
-}
+static size_t get_index(const size_t stride, const int x, const int y) { return y * stride + x; }
 
 Out aoc::y2015::day03(const In &in) {
-  std::unordered_set<Point> part1{std::make_pair(0, 0)};
-  std::unordered_set<Point> part2{std::make_pair(0, 0)};
+  // This makes an assumption about the input, but it's been safe for all inputs I've seen.
+  const auto N = 200;
+  const auto stride = 2 * N + 1;
+  std::bitset<stride * stride> part1, part2;
 
-  Point s1{0, 0}, s2{0, 0}, s3{0, 0};
+  auto s1 = get_index(stride, N, N);
+  auto s2 = s1;
+  auto s3 = s1;
+
+  part1[s1] = true;
+  part2[s2] = true;
 
   bool sturn = true;
-  for (auto &c : in) {
-    s1 = perform_move(s1, c);
-    part1.emplace(s1);
+  for (auto c = in.begin(); c != std::prev(in.cend()); c++) {
+    size_t offset = 0;
+    switch (*c) {
+    case '>':
+      offset = 1;
+      break;
+    case '<':
+      offset = -1;
+      break;
+    case '^':
+      offset = -stride;
+      break;
+    case 'v':
+      offset = stride;
+      break;
+    }
+
+    s1 += offset;
+    part1[s1] = true;
 
     if (sturn) {
-      s2 = perform_move(s2, c);
-      part2.emplace(s2);
+      s2 += offset;
+      part2[s2] = true;
     } else {
-      s3 = perform_move(s3, c);
-      part2.emplace(s3);
+      s3 += offset;
+      part2[s3] = true;
     }
 
     sturn = !sturn;
   }
 
-  return Out{fmt::to_string(part1.size()), fmt::to_string(part2.size())};
+  return Out{fmt::to_string(part1.count()), fmt::to_string(part2.count())};
 }
