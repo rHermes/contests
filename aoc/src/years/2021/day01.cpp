ee0d38c6986f2b72423b94631188867909a99129
Teodor Spæren <teodor@sparen.no>
2021-12-03 12:29:06 +0100

    2021 Day 1
    I am trying to do this with just lexiographical comparisons, but it's
    not going as fast as I think it should. I'll attempt to convert them to
    integers and see if that speeds it up.

--- aoc/src/years/2021/day01.cpp
+++ aoc/src/years/2021/day01.cpp
@@ -1,9 +1,45 @@
 #include "days.hpp"
 #include <fmt/format.h>
+#include <algorithm>
+#include <charconv>
 
 using aoc::In;
 using aoc::Out;
 
+
+// returns True if a < b
+static constexpr bool comp(const std::string_view &a, const std::string_view &b) {
+	if (a.length() != b.length()) {
+		return a.length() < b.length();
+	}
+	return std::lexicographical_compare(a.cbegin(), a.cend(), b.cbegin(), b.cend());
+}
+
 Out aoc::y2021::day01(const In &in) {
-	return {"", ""};
+	std::string_view inp = in;
+
+	std::string_view a{"999999"}, b{"999999"}, c{"999999"};
+
+	std::size_t part1 = 0;
+	std::size_t part2 = 0;
+
+	while (!inp.empty()) {
+		const std::string_view d = inp.substr(0, inp.find(0xa));
+		inp = inp.substr(d.size()+1);
+
+		if (comp(c, d)) {
+			part1++;
+		}
+
+		if (comp(a, d)) {
+			part2++;
+		}
+
+		a = b;
+		b = c;
+		c = d;
+	}
+
+
+  return {fmt::to_string(part1), fmt::to_string(part2)};
 }
