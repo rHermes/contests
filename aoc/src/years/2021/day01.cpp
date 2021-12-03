5efe405b3f458906445359c432a442d7702aa13e
Teodor Spæren <teodor@sparen.no>
2021-12-03 12:35:43 +0100

    Speedup 2021 Day 1
    Converting to using integers speeded this up a lot.

--- aoc/src/years/2021/day01.cpp
+++ aoc/src/years/2021/day01.cpp
@@ -1,37 +1,33 @@
 #include "days.hpp"
 #include <fmt/format.h>
-#include <algorithm>
 #include <charconv>
 
 using aoc::In;
 using aoc::Out;
 
 
-// returns True if a < b
-static constexpr bool comp(const std::string_view &a, const std::string_view &b) {
-	if (a.length() != b.length()) {
-		return a.length() < b.length();
-	}
-	return std::lexicographical_compare(a.cbegin(), a.cend(), b.cbegin(), b.cend());
-}
-
 Out aoc::y2021::day01(const In &in) {
 	std::string_view inp = in;
 
-	std::string_view a{"999999"}, b{"999999"}, c{"999999"};
+	uint64_t a = 9999999999;
+	uint64_t b = a;
+	uint64_t c = a;
 
 	std::size_t part1 = 0;
 	std::size_t part2 = 0;
 
 	while (!inp.empty()) {
-		const std::string_view d = inp.substr(0, inp.find(0xa));
-		inp = inp.substr(d.size()+1);
+		const std::string_view line = inp.substr(0, inp.find(0xa));
+		inp = inp.substr(line.size()+1);
 
-		if (comp(c, d)) {
+		uint64_t d = 0;
+		std::from_chars(line.data(), line.data() + line.size(), d);
+
+		if (c < d) {
 			part1++;
 		}
 
-		if (comp(a, d)) {
+		if (a < d) {
 			part2++;
 		}
 
