4ca0fc008f78d6551b4e46353174bd12b0583d34
Teodor Spæren <teodor@sparen.no>
2024-02-11 19:57:46 +0100

    Add cco06p3 to the list of items.

---
+++ dmoj/cco06/cco06p3-create-weights.py
@@ -0,0 +1,103 @@
+# This program creates the context weights used in each case
+import fileinput as fi
+import collections as cs
+import itertools as it
+
+def build_tables(input):
+    freq = {}
+    valid_num = [10] + list(range(32,126+1))
+    valid = [chr(v) for v in valid_num]
+    for c in valid:
+        tbl = {k: 0 for k in valid}
+        freq[c] = tbl
+
+    totfreq = {k: 0 for k in valid}
+    for a, b in it.pairwise(input):
+        if a  not in valid or b not in valid:
+            continue
+        freq[a][b] += 1
+        totfreq[a] += 1
+    
+    # Now we just need to create the tables that we will use.
+
+    # Newline = 10 = 0
+    # 32 = 1
+    
+    tables = []
+    mm = 0
+
+    totfreq_tbl = [totfreq[nxt] for nxt in valid]
+    max_tot = max(totfreq_tbl)
+    for i, ctx in enumerate(valid):
+        tbl = []
+        for j, nxt in enumerate(valid):
+            tbl.append(freq[ctx][nxt])
+            # tbl.append(totfreq[nxt])
+
+        max_r = max(tbl)
+        mm = max(max_r, mm)
+
+        # We have a problem here, if there are too few rows, we want to replace it.
+        num_not_zero = sum(x != 0 for x in tbl)
+        if num_not_zero < 5:
+            c = 31 + i
+            if c == 31:
+                c = 10
+
+            # c = chr(c)
+            print("We are boosting row [{}] with identity".format(c))
+            # print("We are replacing row [{}] with identity".format(c))
+
+            for j, p in enumerate(tbl):
+                if p != 0:
+                    tbl[j] += max_tot + p
+                else:
+                    tbl[j] = totfreq_tbl[j]
+
+            # tbl = totfreq_tbl
+
+
+        
+        # tbl = [x+10 for x in tbl]
+        tables.append(tbl)
+
+    print("MAX ELEMENT IS {} Which as a bit length of {}".format(mm, mm.bit_length()))
+
+    # print(tables)
+    # N = len(valid_num)
+    # print(N*N)
+    return tables
+
+
+def format_tables(tables):
+    N = len(tables)
+    ans = ""
+    ans += "constexpr std::array<std::array<std::uint64_t, {}>, {}> FREQ{{{{\n".format(N, N)
+                                                                        # }}}}
+    
+    for row in tables:
+        ans += "{" + ",".join([str(x) for x in row]) + "},\n"
+
+    ans += "}};\n";
+
+
+    return ans
+    
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    files = sys.argv[1:]
+
+    inp = ""
+    for file in files:
+        with open(file, encoding="ascii", errors="replace") as f:
+            inp += f.read()
+
+    # print(inp)
+    tables = build_tables(inp)
+    outp = format_tables(tables)
+    with open("weights.h", "w") as f:
+        print(outp, file=f)
---
+++ dmoj/cco06/cco06p3-tester.py
@@ -0,0 +1,83 @@
+import subprocess as ps
+import math
+
+
+BIN_PLACE = "/home/rhermes/commons/projects/contests/plz-out/bin/dmoj/cco06p3"
+# BIN_PLACE = "/home/rhermes/commons/projects/contests/dmoj/yac8/p5"
+
+def make_sure_its_up_to_date():
+    WL = "/home/rhermes/commons/projects/contests/pleasew"
+
+    pl = ps.run([WL, "build", "//dmoj:cco06p3"], capture_output=True)
+    pl.check_returncode()
+
+# OK so we are going to create some tests here, but 
+def to_problem_format(text):
+    return "1\n" + text
+
+def get_bitstring(text):
+    inp = "1\n" + text
+
+    gv  = ps.run(BIN_PLACE,
+                 input=inp.encode("utf-8"),
+                 capture_output=True,
+                 )
+    gv.check_returncode()
+    return gv.stdout.decode('utf-8')
+
+
+def parse_bitstring(bitstring):
+    inp = "2\n" + bitstring
+
+    gv = ps.run(BIN_PLACE,
+                input=inp.encode("utf-8"),
+                capture_output=True,
+                )
+    gv.check_returncode()
+
+    return gv.stdout.decode('utf-8')
+
+
+def perform_test(text):
+    bs = get_bitstring(text)
+    parsed = parse_bitstring(bs)
+    assert(text == parsed)
+
+    return len(bs)
+
+def eval_file(fp):
+
+    text = ""
+    with open(fp, encoding="ascii", errors="replace") as f:
+        text = f.read()
+
+    # we just clean up the input now
+    lp = list(text)
+    for i,c in enumerate(lp):
+        if not (ord(c) == 10 or 32 <= ord(c) <= 126):
+            lp[i] = '?'
+
+    
+    text = "".join(lp)
+    bsLen = perform_test(text)
+    rat = bsLen/len(text)
+    score = min(100, 50 * math.sqrt(8 - rat))
+
+    # print("Evauluating {}: Bitstring was {} long, having a ratio of {}, meaning we got a score of {}".format(
+    #     fp, bsLen, rat, score,))
+    print("Evauluating {:10}: ratio of {:.4f}, score of {:6.2f}".format(fp, rat, score,))
+
+if __name__ == "__main__":
+    make_sure_its_up_to_date()
+
+    import sys
+    files = sys.argv[1:]
+    try:
+        import natsort
+        # files = natsort.natsorted(files)
+        files = natsort.os_sorted(files)
+    except:
+        pass
+
+    for file in files:
+        eval_file(file)
