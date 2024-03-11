c6bd0012e0f5da4bc43ea36ccd8ba654d78e90bf
Teodor Spæren <teodor@sparen.no>
2024-03-11 20:23:36 +0100

    Fix up the E submission

--- codeforces/1941/c1941pE.cpp
+++ codeforces/1941/c1941pE.cpp
@@ -8,9 +8,89 @@
 #include <unordered_map>
 #include <algorithm>
 #include <string>
+#include <deque>
 
 using UT = std::uint64_t;
 using ST = std::int64_t;
+
+template <typename T>
+class RollingMinSum {
+	T ans_{std::numeric_limits<T>::max()};
+	T cur_{0};
+	const std::size_t cap_;
+	std::vector<T> data_;
+	std::size_t idx_{0};
+	bool wrapped_{false};
+	
+	public:
+
+	explicit RollingMinSum(std::size_t cap) : cap_{cap}, data_(cap_) {}
+
+	[[nodiscard]] constexpr const T& ans() const {
+		return ans_;
+	}
+
+	[[nodiscard]] constexpr const T& cur() const {
+		return cur_;
+	}
+
+	constexpr void add(T val) {
+		if (wrapped_) {
+			cur_ -= data_[idx_];
+		}
+
+		cur_ += val;
+		data_[idx_] = std::move(val);
+
+		idx_++;
+		if (idx_ == cap_) {
+			idx_ = 0;
+			wrapped_ = true;
+		}
+		if (wrapped_)
+			ans_ = std::min(ans_, cur_);
+	}
+};
+
+template <typename T>
+class RollingMin {
+	std::deque<std::pair<T,std::size_t>> q_;
+	std::size_t removed_{0};
+	std::size_t added_{0};
+	const std::size_t capacity_;
+
+	constexpr void pop() {
+		if (!q_.empty() && q_.front().second == removed_)
+			q_.pop_front();
+
+		removed_++;
+	}
+
+public:
+	
+	explicit RollingMin(std::size_t cap) : capacity_{cap} {}
+
+	[[nodiscard]] constexpr T& min() {
+		return q_.front().first;
+	}
+
+	[[nodiscard]] constexpr const T& min() const {
+		return q_.front().first;
+	}
+
+	constexpr void push(T val) {
+		if (capacity_ <= added_)
+			pop();
+
+		while (!q_.empty() && val < q_.back().first)
+			q_.pop_back();
+
+		q_.emplace_back(std::move(val), added_);
+
+		added_++;
+	}
+
+};
 
 void solve() {
 	UT N = 0;
@@ -20,70 +100,47 @@ void solve() {
 	std::cin >> N >> M >> K >> D;
 
 	std::vector<UT> row(M);
-	std::vector<UT> dp(M);
-	std::vector<UT> bridgeCosts(N);
+	/* std::vector<UT> dp(M); */
 
+	RollingMinSum<UT> ans(K);
 	for (UT i = 0; i < N; i++) {
 		for (UT j = 0; j < M; j++) {
 			std::cin >> row[j];
+			row[j]++;
 		}
 
 		// We have scanned in the row now.
 		// Ok, so I want to reduce the amount of cases, to make this easier on my brain.
-		//
+
 		// We always have supports at 0 and M-1
 		if (M-1 <= D+1) {
 			// We won't need any struts.
-			bridgeCosts[i] = row[0] + row[M-1] + 1 + 1;
+			ans.add(row[0] + row[M-1]);
 			continue;
 		}
 
-		// Ok, so we cannot reach all the way from the end to the start.
-		// We will begin with the end.
-		dp[M-1] = row[M-1]+1;
-		
-		// OK, so we know that for all which M-1 <= x + D + 1, the answer is just the row
-		// plus the end.
+
 		// M-1-D-1
 		for (UT i = M-1-D-1; i < M-1; i++) {
-			dp[i] = row[i]+1 + dp[M-1];
+			row[i] += row[M-1];
 		}
 
-		// Ok now our upper limit is M-1-D-1
 		UT upperLimit = M-1-D-1;
+		RollingMin<UT> mm(D+1);
+		for (UT i = M-1; upperLimit <= i; i--)
+			mm.push(row[i]);
+
 		// But we also have a lower limit:
 		for (UT i = upperLimit; 0 < i; i--) {
 			UT ii = i-1;
-			UT best = dp[ii+1];
-			// we pick out the smallest of the next x.
-			for (UT j = 2; j <= D+1; j++) {
-				best = std::min(best, dp[ii+j]);
-			}
-			dp[ii] = row[ii]+1+best;
+			row[ii] += mm.min();
+			mm.push(row[ii]);
 		}
 
-		// ok, now dp[0], the almighty.
-		bridgeCosts[i] = dp[0];
+		ans.add(row[0]);
 	}
 
-	/* std::cout << "BRIDGE COSTS" << std::endl; */
-	/* for (const auto& p : bridgeCosts) { */
-	/* 	std::cout << p << std::endl; */
-	/* } */
-	/* std::cout << "END IT " << std::endl; */
-	
-	UT ans = std::numeric_limits<UT>::max();
-	for (UT i = 0; i <= N-K; i++) {
-		UT thisAns = 0;
-		for (UT j = 0; j < K; j++) {
-			thisAns += bridgeCosts[i+j];
-		}
-
-		ans = std::min(thisAns, ans);
-	}
-
-
-	std::cout << ans << std::endl;
+	std::cout << ans.ans() << std::endl;
 }
 
 int main() {
