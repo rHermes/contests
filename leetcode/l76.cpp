5cb9a099cb30e08cae142a72f4591d1e06b892c2
Teodor Spæren <teodor@sparen.no>
2024-03-15 12:27:18 +0100

    temp checkpoint

---
+++ leetcode/l76.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <set>
+#include <vector>
+
+class Solution {
+public:
+	std::string minWindow(std::string s, std::string t) {
+		// ok, this is my strategy. I will use two pointers.
+		// when I find all needed strings, I will stop, and then
+		// continnue going forward until I have all the needed elements.
+	}
+};
