44e4d0ae86bf444890d9a9d36a06aad09b7c4918
Teodor Spæren <teodor@sparen.no>
2024-03-19 09:42:57 +0100

    More leetcode

---
+++ leetcode/l1381.cpp
@@ -0,0 +1,48 @@
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+class CustomStack {
+  std::size_t cap_;
+  std::vector<int> data_;
+
+public:
+  CustomStack(int maxSize) : cap_(static_cast<std::size_t>(maxSize)) { data_.reserve(cap_); }
+
+  void push(int x) {
+    if (data_.size() < cap_)
+      data_.emplace_back(x);
+  }
+
+  int pop() {
+    if (data_.empty())
+      return -1;
+
+    auto a = data_.back();
+    data_.pop_back();
+    return a;
+  }
+
+  void increment(int k, int val) {
+    auto level = std::min(static_cast<std::size_t>(k), data_.size());
+    for (std::size_t i = 0; i < level; i++)
+      data_[i] += val;
+  }
+};
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * CustomStack* obj = new CustomStack(maxSize);
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * obj->increment(k,val);
+ */
+
+static const auto __optimize__ = []() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  return 0;
+}();
+
+int main() { return 0; }
---
+++ leetcode/l2502.cpp
@@ -0,0 +1,71 @@
+#include <algorithm>
+#include <unordered_map>
+#include <vector>
+
+class Allocator {
+  std::vector<std::pair<std::size_t, std::size_t>> freeBlocks_;
+  std::unordered_map<int, std::vector<std::pair<std::size_t, std::size_t>>> takenBlocks_;
+
+  void merge() {
+    if (freeBlocks_.size() < 2)
+      return;
+
+    for (auto i = freeBlocks_.size() - 1; 0 < i; i--) {
+      auto ii = i - 1;
+
+      auto &[al, ar] = freeBlocks_[ii];
+      auto &[bl, br] = freeBlocks_[i];
+      if (ar == bl) {
+        ar = br;
+
+        freeBlocks_.erase(freeBlocks_.begin() + static_cast<std::ptrdiff_t>(i));
+      }
+    }
+  }
+
+public:
+  Allocator(int n) { freeBlocks_.emplace_back(0, n); }
+
+  int allocate(int size, int mID) {
+    const auto sz = static_cast<std::size_t>(size);
+
+    for (auto it = freeBlocks_.begin(); it != freeBlocks_.end(); it++) {
+      auto &[l, r] = *it;
+
+      if (sz <= (r - l)) {
+        takenBlocks_[mID].emplace_back(l, l + sz);
+        auto old = l;
+        l += sz;
+
+        return static_cast<int>(old);
+      }
+    }
+
+    return -1;
+  }
+
+  int free(int mID) {
+    auto it = takenBlocks_.find(mID);
+    if (it == takenBlocks_.end())
+      return 0;
+
+    std::size_t ans = 0;
+    for (const auto &p : it->second) {
+      ans += p.second - p.first;
+      const auto spot = std::upper_bound(freeBlocks_.begin(), freeBlocks_.end(), p);
+      freeBlocks_.insert(spot, p);
+    }
+
+    takenBlocks_.erase(it);
+    merge();
+    return static_cast<int>(ans);
+  }
+};
+
+/**
+ * Your Allocator object will be instantiated and called as such:
+ * Allocator* obj = new Allocator(n);
+ * int param_1 = obj->allocate(size,mID);
+ * int param_2 = obj->free(mID);
+ */
+int main() { return 0; }
