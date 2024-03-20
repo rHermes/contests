ffa450be3693ff85ceab334554a736bf75e1a9d9
Teodor Spæren <teodor@sparen.no>
2024-03-20 12:37:20 +0100

    Improve l76

--- leetcode/l76.cpp
+++ leetcode/l76.cpp
@@ -1,84 +1,48 @@
 #include <string>
-#include <set>
 #include <vector>
-#include <unordered_map>
 #include <algorithm>
+#include <cstdint>
+#include <limits>
 
 class Solution {
-	std::string_view canBeDone(const std::string_view s,
-														const std::unordered_map<char, int>& needed,
-														const std::size_t size) {
-		auto wanted = needed;
-
-		std::size_t head = 0;
-		for (std::size_t i = 0; i < size; i++) {
-			if (auto it = wanted.find(s[i]); it != wanted.end())
-				it->second--;
-		}
-
-		auto done = std::all_of(wanted.cbegin(), wanted.cend(),
-													[](const auto& a) { return a.second <= 0; });
-
-		if (done) {
-			return s.substr(head, size);
-		}
-
-		while (head < (s.size() - size)) {
-			if (auto it = wanted.find(s[head]); it != wanted.end())
-				it->second++;
-
-			if (auto it = wanted.find(s[head + size]); it != wanted.end()) {
-				head++;
-				// ok, here we are.
-				it->second--;
-				if (0 != it->second) {
-					continue;
-				}
-
-				auto done =
-					std::all_of(wanted.cbegin(), wanted.cend(),
-								 [](const auto& a) { return a.second <= 0; });
-
-				if (done) {
-					return s.substr(head, size);
-				}
-
-				continue;
-			}
-
-			head++;
-		}
-
-		return {};
-	}
-
 public:
 	std::string minWindow(std::string s, std::string t) {
-		std::unordered_map<char, int> needed;
+		std::int64_t count = static_cast<std::int64_t>(t.size());
+		std::vector<std::int32_t> needed('z' - 'A' + 1);
+
 		for (const auto c : t)
-		needed[c]++;
+			needed[static_cast<std::size_t>(c - 'A')]--;
 
-		auto best = canBeDone(s, needed, s.size());
-		if (best.empty()) {
-			return "";
-		}
+		std::size_t l = 0;
+		std::size_t r = 0;
 
-		std::size_t L = 0;
-		std::size_t R = s.size();
+		std::size_t minLen = std::numeric_limits<std::size_t>::max();
+		std::size_t startIndex = 0;
 
-		while (L + 1 < R) {
-			auto C = (L + R) / 2;
-			auto top = canBeDone(s, needed, C);
-			if (top.empty()) {
-				L = C;
-			} else {
-				best = top;
-				R = C;
+		while (r < s.size()) {
+			auto found = needed[static_cast<std::size_t>(s[r] - 'A')]++;
+			r++;
+			if (found < 0)
+				count--;
+
+			while (count == 0) {
+				auto found = needed[static_cast<std::size_t>(s[l] - 'A')]--;
+				l++;
+				if (found == 0) {
+					count++;
+
+					if ((r - l + 1) < minLen) {
+						startIndex = l - 1;
+						minLen = r - l + 1;
+					}
+				}
 			}
 		}
 
-		// Ok, so we know
-		return std::string(best);
+		if (minLen == std::numeric_limits<std::size_t>::max())
+			return "";
+		else
+			return s.substr(startIndex, minLen);
 	}
 };
 
