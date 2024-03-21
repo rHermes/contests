06e2c7ff69dbeb7d891cf14a38b6f9361f3c85d7
Teodor Spæren <teodor@sparen.no>
2024-03-21 18:51:11 +0100

    More leetcode in the car

---
+++ leetcode/l461.cpp
@@ -0,0 +1,12 @@
+#include <bitset>
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        std::bitset<sizeof(int) * 8> xx(static_cast<unsigned long>(x));
+        std::bitset<sizeof(int) * 8> yy(static_cast<unsigned long>(y));
+        return static_cast<int>((xx ^ yy).count());
+    }
+};
+
+int main() { return 0; }
