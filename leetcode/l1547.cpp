08f54758611268db8d9f9b69e15edd985aa939c3
Teodor Spæren <teodor@sparen.no>
2024-03-25 15:45:45 +0100

    New leetcode tasks

---
+++ leetcode/l1547.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <cstdint>
+#include <limits>
+#include <vector>
+
+class Solution {
+public:
+  int minCost(int n, std::vector<int> &cuts) {
+    std::sort(cuts.begin(), cuts.end());
+
+    // We are virtually adding 0 in the front and n in the back.
+    const auto N = cuts.size() + 2;
+
+    // and we are going to store it in one block, for better locality
+    std::vector<std::int32_t> dp(N * N, 0);
+
+    // dp[l][r] == best possible solution when cut starts at cuts[l] and ends at cuts[r].
+    for (std::size_t L = N - 2; 0 < L; L--) {
+      const auto LL = L - 1;
+      const auto LLcut = (LL == 0) ? 0 : cuts[LL - 1];
+
+      for (std::size_t RR = LL + 2; RR < N; RR++) {
+        auto ans = std::numeric_limits<std::int32_t>::max();
+
+        for (std::size_t PL = LL + 1; PL < RR; PL++) {
+          ans = std::min(ans, dp[LL * N + PL] + dp[PL * N + RR]);
+        }
+
+        const auto RRcut = (RR == N - 1) ? n : cuts[RR - 1];
+        dp[LL * N + RR] = ans + (RRcut - LLcut);
+      }
+    }
+
+    // Then we just return [0,N-1]
+    return dp[0 * N + N - 1];
+  }
+};
+
+int main() { return 0; }
---
+++ leetcode/l2147.cpp
@@ -0,0 +1,50 @@
+#include <cstdint>
+#include <iostream>
+#include <string>
+
+class Solution {
+public:
+  int numberOfWays(std::string corridor) {
+    // ok, so we have a mission here.
+    const auto N = corridor.size();
+    constexpr std::int64_t MODN = 1000000007;
+
+    std::int64_t answer = 1;
+
+    // we are just looking for runs of bushes.
+    std::int8_t chairsSeen = 0;
+    std::int32_t plantsSeen = 0;
+    for (std::size_t i = 0; i < N; i++) {
+      if (corridor[i] == 'S') {
+        chairsSeen++;
+
+        if (chairsSeen == 3) {
+          // ok, so we are going to put a divider one before this.
+          answer = (answer * (plantsSeen + 1)) % MODN;
+
+          chairsSeen = 1;
+        } else if (chairsSeen == 2) {
+          // ok, now we have seen two chairs. we reset the number of
+          // plants seen.
+          plantsSeen = 0;
+        }
+      } else {
+        plantsSeen++;
+      }
+    }
+
+    if (chairsSeen != 2)
+      return 0;
+    else
+      return static_cast<int>(answer);
+  }
+};
+
+const int init = []() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(0);
+  std::cout.tie(0);
+  return 0;
+}();
+
+int main() { return 0; }
