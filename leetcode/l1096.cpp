0a3bd811b67ddaa41c32c2034043b2427e1cf94b
Teodor Spæren <teodor@sparen.no>
2024-03-28 12:03:02 +0100

    Add more leetcode problems for the day

---
+++ leetcode/l1096.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <set>
+#include <string>
+#include <string_view>
+#include <vector>
+
+class Solution {
+public:
+  // The reason we use a sorted set, is that the merge performance is better.
+  // An alternative here, for smaller sizes, would just be to use vectors for
+  // everything and maintain the invariants at the start and end of the expansions
+  // this could work, and given the layout of a vector, would probabily give us
+  // a better cache locality. I've optimized for the common case here, where we
+  // don't know the set sizes.
+  using set_type = std::set<std::string>;
+
+  void carts(set_type &dst, set_type &src) {
+    if (src.empty())
+      return;
+
+    if (dst.empty()) {
+      dst = std::move(src);
+      return;
+    }
+
+    set_type cart;
+    for (const auto &a : dst)
+      for (const auto &b : src)
+        cart.insert(a + b);
+
+    dst = std::move(cart);
+  }
+
+  // This is devious, but
+  set_type expand(std::string_view &current) {
+    set_type accumulated;
+
+    // We are going to be scanning this one element at a time.
+    if (!current.starts_with('{'))
+      throw std::runtime_error("We should always give a full capture for expansion");
+
+    current.remove_prefix(1);
+
+    // ok, so we keep iterating until we see our first '}'
+    while (!current.starts_with('}')) {
+      // ok, now we should always be at either
+      set_type cum; // this is for this element.
+
+      while (!current.starts_with(',') && !current.starts_with('}')) {
+        // std::cout << "We are entering inner with: " << current <<
+        // std::endl;
+        //  ok, so we have procecced one element
+        if (current.starts_with('{')) {
+          auto subexpand = expand(current);
+          carts(cum, subexpand);
+        } else {
+          // ok, now we are going to keep scanning until we find a ','
+          auto end = current.find_first_of(",{}");
+          // We just iterate over and add to the queue.
+          set_type gotit;
+          gotit.emplace(current.substr(0, end));
+
+          carts(cum, gotit);
+
+          current.remove_prefix(end);
+        }
+      }
+
+      // At the end of this element, we just merge the two.
+      accumulated.merge(cum);
+
+      if (current.starts_with(',')) {
+        current.remove_prefix(1);
+      }
+    }
+
+    // We pop the end square bracket
+    current.remove_prefix(1);
+
+    return accumulated;
+  }
+
+  std::vector<std::string> braceExpansionII(std::string expression) {
+    // I can only expand full braces, so we wrap the expression in one,
+    // to be sure we handle all cases.
+    expression = '{' + expression + '}';
+    std::string_view view(expression);
+    // ok ok ok, here we go.
+    const auto goods = expand(view);
+
+    std::vector<std::string> ret(goods.begin(), goods.end());
+
+    return ret;
+  }
+};
+
+static const auto __optimize__ = []() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  return 0;
+}();
+
+int main() { return 0; }
---
+++ leetcode/l1377.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+  using graph_type = std::vector<std::vector<int>>;
+
+  double frogPosition(int n, std::vector<std::vector<int>> &edges, int t, const int target) {
+    // convert to a good graph
+    graph_type G(n);
+
+    // We assume here that the edges are only given once.
+    for (const auto &pair : edges) {
+      G[pair[0] - 1].push_back(pair[1] - 1);
+      G[pair[1] - 1].push_back(pair[0] - 1);
+    }
+
+    // We do a BFS. in general we will hit our target this way, with a
+    // higher base latency.
+    std::vector<double> prob(n);
+    prob[0] = 1.0;
+
+    std::vector<bool> visited(n);
+    visited[0] = true;
+
+    // We are using two vectors here and swap between them, for each layer.
+    // this makes it easier to process.
+    std::vector<int> next;
+    std::vector<int> current{0};
+
+    while (0 < t && !current.empty()) {
+      // We process them 1 step at a time.
+      for (const auto p : current) {
+        // The root has no parents
+        const auto posNext = G[p].size() - (p != 0);
+        const auto nextProbs = 1.0 / static_cast<double>(posNext) * prob[p];
+
+        for (const auto child : G[p]) {
+          if (visited[child])
+            continue;
+
+          visited[child] = true;
+          prob[child] = nextProbs;
+          next.push_back(child);
+        }
+
+        // we have jumped past this.
+        if (0 < posNext)
+          prob[p] = 0;
+      }
+
+      // swap the current and the next
+      using std::swap;
+      swap(next, current);
+      next.clear();
+
+      // decrement the time after 1 jump.
+      t--;
+    }
+
+    return prob[target - 1];
+  }
+};
+
+int main() { return 0; }
