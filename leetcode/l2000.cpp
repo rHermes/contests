796aa89d6388b33475e1078cf3d0a6805dbe0218
Teodor Spæren <teodor@sparen.no>
2024-05-01 14:28:39 +0200

    Daily leetcode

---
+++ leetcode/l2000.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <algorithm>
+
+class Solution {
+public:
+	std::string reversePrefix(std::string word, char ch) {
+    const auto pos = word.find(ch);
+    if (pos != word.npos) {
+      reverse(word.begin(), word.begin() + pos + 1);
+    }
+
+    return word;
+  }
+};
+
+int main() { return 0; }
