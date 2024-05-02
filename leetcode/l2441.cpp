ef1425b432deead52a49af68ac539f2512aefa0e
Teodor Spæren <teodor@sparen.no>
2024-05-02 10:58:20 +0200

    Daily leetcode

---
+++ leetcode/l2441.cpp
@@ -0,0 +1,34 @@
+#include <bitset>
+#include <vector>
+
+class Solution {
+public:
+  int findMaxK(std::vector<int> &nums) {
+    constexpr std::size_t MID = 1000;
+    std::bitset<MID + 1> pos;
+    std::bitset<MID + 1> neg;
+
+    int ans = -1;
+    for (const auto &num : nums) {
+      if (0 <= num) {
+        pos[num] = true;
+
+        // we check if the positive is there
+        if (neg[num] && ans < num) {
+          ans = num;
+        }
+      } else {
+        const auto flip = -num;
+        neg[flip] = true;
+
+        if (pos[flip] && ans < (flip)) {
+          ans = flip;
+        }
+      }
+    }
+
+    return ans;
+  }
+};
+
+int main() { return 0; }
