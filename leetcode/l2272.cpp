181aa52f023b86efe3f24c9e58182e403c9f9ef5
Teodor Spæren <teodor@sparen.no>
2024-05-03 20:24:40 +0200

    Optimize 2272 leetcode

--- leetcode/l2272.cpp
+++ leetcode/l2272.cpp
@@ -1,64 +1,61 @@
-#include <string>
 #include <array>
+#include <string>
+#include <cstdint>
 
 class Solution {
 public:
-  int largestVariance(const std::string& s) {
-    std::array<int, 26> counts{};
-    for (const auto c : s)
-      counts[c - 'a']++;
+  int largestVariance(const std::string &s) {
+
+    // Ok, we are creating a info struct.
+    struct Info {
+      std::int16_t major{0};
+      std::int16_t minor{0};
+      bool flag{false};
+    };
+
+    // Making a wrapped array.
+    std::array<std::array<Info, 26>, 26> DP{};
 
     int ans = 0;
-    // ok, now we repeat Kadanes algortihm caross.
-    for (int i = 0; i < 26; i++) {
-      for (int j = 0; j < 26; j++) {
-        if (i == j || !counts[i] || !counts[j])
+    for (const auto letter : s) {
+      const int c = letter - 'a';
+
+      // Update all infos where c is a minor.
+      for (int i = 0; auto &info : DP) {
+        if (c == i++)
           continue;
 
-        // ok, so we have a major and minor one at this point. Let's
-        // think a bit.
+        // ok, so we are using the C as the major now.
+        auto &[major, minor, flag] = info[c];
 
-        int MajorLeft = counts[i];
-        int MinorLeft = counts[j];
+        // Flag here indicate that we come from a negative state.
+        if (flag) {
+          minor--;
+          flag = false;
+        }
 
-        int major_count = 0;
-        int minor_count = 0;
-        for (const auto cc : s) {
-          const int c = cc - 'a';
+        minor++;
+        if (major < minor) {
+          major = 0;
+          minor = 1;
+          flag = true;
+        } else {
+          ans = std::max(ans, major - minor);
+        }
+      }
 
-          if (c == i) {
-            major_count++;
+      // Update C info, where c is the major
+      for (int i = 0; auto &[major, minor, flag] : DP[c]) {
+        if (c == i++)
+          continue;
 
-            if (0 < minor_count)
-              ans = std::max(major_count - minor_count, ans);
-
-            MajorLeft--;
-            if (MajorLeft == 0) {
-              if (minor_count == 0)
-                ans = std::max(major_count - 1, ans);
-
-              break;
-            }
-          } else if (c == j) {
-            minor_count++;
-
-            if (minor_count == 1)
-              ans = std::max(major_count - minor_count, ans);
-
-            MinorLeft--;
-            if (MinorLeft == 0) {
-              ans = std::max(ans, major_count - minor_count + MajorLeft);
-              break;
-            }
-
-            if (major_count < minor_count) {
-              minor_count = 0;
-              major_count = 0;
-            }
-          }
+        major++;
+        if (minor != 0) {
+          ans = std::max(ans, major - minor);
         }
       }
     }
+
     return ans;
   }
 };
