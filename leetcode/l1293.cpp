3d2b023424f8cd0be24288ed5a0d44c0df49d0d9
Teodor Spæren <teodor@sparen.no>
2024-05-07 01:50:03 +0200

    More leetcode

---
+++ leetcode/l1293.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+
+inline const auto optimize = []() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  return 0;
+}();
+
+class Solution {
+public:
+  static int shortestPath(const std::vector<std::vector<int>> &grid, int k) {
+    const int ROWS = grid.size();
+    const int COLS = grid[0].size();
+
+    std::vector<int> left(ROWS * COLS, -1);
+
+    std::vector<std::tuple<int, int, int>> Q;
+    std::vector<std::tuple<int, int, int>> next;
+
+    const auto addIt = [&](const int y, const int x, const int l) {
+      if (l <= left[y * COLS + x])
+        return;
+
+      if (grid[y][x]) {
+        if (l && left[y * COLS + x] < l - 1)
+          next.emplace_back(y, x, l - 1);
+      } else {
+        next.emplace_back(y, x, l);
+      }
+    };
+
+    Q.emplace_back(0, 0, k);
+
+    int steps = 0;
+    while (!Q.empty()) {
+      next.clear();
+
+      for (const auto &[y, x, l] : Q) {
+        if (l <= left[y * COLS + x])
+          continue;
+
+        left[y * COLS + x] = l;
+
+        if (y == ROWS - 1 && x == COLS - 1)
+          return steps;
+
+        if (0 < y)
+          addIt(y - 1, x, l);
+
+        if (y < ROWS - 1)
+          addIt(y + 1, x, l);
+
+        if (0 < x)
+          addIt(y, x - 1, l);
+
+        if (x < COLS - 1)
+          addIt(y, x + 1, l);
+      }
+
+      std::swap(Q, next);
+      steps++;
+    }
+
+    return -1;
+  }
+};
+
+int main() { return 0; }
---
+++ leetcode/l528.cpp
@@ -0,0 +1,43 @@
+#include <cstdint>
+#include <iostream>
+#include <limits>
+#include <random>
+#include <vector>
+
+struct pcg32_random_r {
+  std::uint64_t state;
+  std::uint64_t inc;
+
+  using result_type = std::uint32_t;
+
+  static result_type min() { return 0; }
+  static result_type max() { return std::numeric_limits<result_type>::max(); }
+
+  result_type operator()() {
+    std::uint64_t oldstate = state;
+    // Advance internal state
+    state = oldstate * 6364136223846793005ULL + (inc | 1);
+    // Calculate output function (XSH RR), uses old state for max ILP
+    std::uint32_t xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
+    std::uint32_t rot = oldstate >> 59u;
+    return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
+  }
+};
+
+inline const auto optimize = []() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  return 0;
+}();
+
+class Solution {
+public:
+  pcg32_random_r rng;
+  std::discrete_distribution<int> dist;
+  Solution(std::vector<int> &w) : rng{123, 123}, dist(w.begin(), w.end()) {}
+
+  int pickIndex() { return dist(rng); }
+};
+
+int main() { return 0; }
