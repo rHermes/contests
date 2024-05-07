adf3a420b4574daa8c0c12b0c0066d8b61ab9dd0
Teodor Spæren <teodor@sparen.no>
2024-05-08 01:09:09 +0200

    Optimize a little format

--- leetcode/l1396.cpp
+++ leetcode/l1396.cpp
@@ -1,38 +1,59 @@
+#include <cstddef>
+#include <cstdint>
+#include <iostream>
 #include <string>
+#include <unordered_map>
 #include <utility>
-#include <map>
-#include <cinttypes>
-class UndergroundSystem {
-public:
-    std::map<int,std::pair<std::string,int>> inFlight;
-    std::map<
-    std::pair<std::string,std::string>,
-    std::pair<std::uintmax_t,std::uintmax_t>
-    > timings{};
 
-    UndergroundSystem() {
-    }
-    
-    void checkIn(int id, std::string stationName, int t) {
-        inFlight.emplace(std::piecewise_construct,
-        std::forward_as_tuple(id),
-        std::forward_as_tuple(stationName, t)
-        );
-    }
-    
-    void checkOut(int id, std::string stationName, int t) {
-        auto nh = inFlight.extract(id);
-        const auto& [src, srcT] = nh.mapped();
-        auto& lel = timings[{src, stationName}];
-        lel.first += static_cast<std::uintmax_t>(t-srcT);
-        lel.second += 1;
-    }
-    
-    double getAverageTime(std::string startStation, std::string endStation) {
-        const auto& [sum, num] = timings[{startStation, endStation}];
-        return static_cast<double>(sum) / static_cast<double>(num);
-    }
-};
+inline const auto optimize = []() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  return 0;
+}();
 
-int main() {
+template<typename T, typename... Rest>
+void
+hash_combine(std::size_t& seed, const T& v, const Rest&... rest)
+{
+  seed ^= std::hash<T>{}(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+  (hash_combine(seed, rest), ...);
 }
+
+class UndergroundSystem
+{
+  using TimingsVal = std::pair<std::uintmax_t, std::uintmax_t>;
+
+public:
+  std::unordered_map<int, std::pair<std::string, int>> inFlight;
+  std::unordered_map<std::size_t, TimingsVal> timings;
+
+  void checkIn(int id, std::string stationName, int t)
+  {
+    inFlight.emplace(
+      std::piecewise_construct, std::forward_as_tuple(id), std::forward_as_tuple(std::move(stationName), t));
+  }
+
+  void checkOut(int id, const std::string& stationName, int t)
+  {
+    auto nh                 = inFlight.extract(id);
+    const auto& [src, srcT] = nh.mapped();
+
+    std::size_t seed = 0;
+    hash_combine(seed, src, stationName);
+
+    auto& lel = timings[seed];
+    lel.first += (t - srcT);
+    lel.second += 1;
+  }
+
+  double getAverageTime(const std::string& startStation, const std::string& endStation)
+  {
+
+    std::size_t seed = 0;
+    hash_combine(seed, startStation, endStation);
+
+    const auto& [sum, num] = timings[seed];
+    return static_cast<double>(sum) / num;
+  }
+};
