09838c55126005379984902ec3b3fda11a505d75
Teodor Spæren <teodor@sparen.no>
2024-05-08 18:48:48 +0200

    More leetcode

---
+++ leetcode/l1444.cpp
@@ -0,0 +1,112 @@
+#include <array>
+#include <cstdint>
+#include <iostream>
+#include <string>
+#include <vector>
+
+constexpr int MODN = 1000000007;
+
+inline const auto optimize = []() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  return 0;
+}();
+
+class Solution
+{
+private:
+  std::array<std::int16_t, 51 * 51> PSA;
+  std::array<int, 51 * 51 * 10> MEMO;
+
+  std::size_t ROWS;
+  std::size_t COLS;
+  std::size_t PROWS;
+  std::size_t PCOLS;
+
+  std::size_t K;
+
+  int solve(int y, int x, int k)
+  {
+    const auto totalLeft = PSA[PCOLS * y + x];
+
+    if (totalLeft <= k) {
+      return 0;
+    }
+
+    if (k == 0) {
+      return 1;
+    }
+
+    const std::size_t key = y * (COLS * K) + x * K + k;
+    if (MEMO[key] != -1)
+      return MEMO[key];
+
+    std::int64_t ans = 0;
+    for (std::size_t ny = y + 1; ny < ROWS; ny++) {
+      const auto leftAbove = totalLeft - PSA[ny * PCOLS + x];
+
+      if (leftAbove <= 0)
+	continue;
+
+      if (leftAbove == totalLeft)
+	break;
+
+      ans = (ans + solve(ny, x, k - 1)) % MODN;
+    }
+
+    for (std::size_t nx = x + 1; nx < COLS; nx++) {
+      const auto leftLeft = totalLeft - PSA[y * PCOLS + nx];
+
+      if (leftLeft <= 0)
+	continue;
+
+      if (leftLeft == totalLeft)
+	break;
+
+      ans = (ans + solve(y, nx, k - 1)) % MODN;
+    }
+
+    MEMO[key] = ans;
+    return ans;
+  };
+
+public:
+  int ways(const std::vector<std::string>& pizza, int k)
+  {
+    ROWS  = pizza.size();
+    COLS  = pizza[0].size();
+    PCOLS = COLS + 1;
+    PROWS = ROWS + 1;
+
+    K = k;
+
+    // We need to make sure that the PSA edges are filled in before we
+    // start, as we might be reusing the cache from a previous run.
+    for (std::size_t col = 0; col < COLS; col++) {
+      PSA[ROWS * PCOLS + col] = 0;
+    }
+    for (std::size_t row = 0; row < ROWS; row++) {
+      PSA[row * PCOLS + COLS] = 0;
+    }
+
+    // Ok, we flip the script a little here. We are going to build a PSA
+    // where we are recording the sum to the lower right corner. This fit's
+    // a lot better with the task.
+    for (int row = ROWS - 1; 0 <= row; row--) {
+      for (int col = COLS - 1; 0 <= col; col--) {
+	PSA[row * PCOLS + col] = (pizza[row][col] == 'A') + PSA[(row + 1) * PCOLS + col] + PSA[row * PCOLS + col + 1] -
+	                         PSA[(row + 1) * PCOLS + col + 1];
+      }
+    }
+
+    std::fill_n(MEMO.begin(), ROWS * COLS * K, -1);
+    return solve(0, 0, k - 1);
+  }
+};
+
+int
+main()
+{
+  return 0;
+}
---
+++ leetcode/l1604.cpp
@@ -0,0 +1,66 @@
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+inline const auto optimize = []() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  return 0;
+}();
+
+class Solution
+{
+public:
+  static std::vector<std::string> alertNames(const std::vector<std::string>& keyName,
+                                             const std::vector<std::string>& keyTime)
+  {
+    std::vector<int> indexes(keyName.size());
+    std::iota(indexes.begin(), indexes.end(), 0);
+
+    std::sort(indexes.begin(), indexes.end(), [&](const int i, const int j) { return keyTime[i] < keyTime[j]; });
+
+    // Workers, we are just going to keep the last 3 for each.
+    constexpr auto parseTime = [](const std::string& a) {
+      int hours   = (a[0] - '0') * 10 + (a[1] - '0');
+      int minutes = (a[3] - '0') * 10 + (a[4] - '0');
+
+      return hours * 60 + minutes;
+    };
+
+    std::unordered_set<std::string_view> out;
+
+    std::unordered_map<std::string_view, std::tuple<int, int, int>> db;
+    for (std::size_t i = 0; i < indexes.size(); i++) {
+      const auto idx = indexes[i];
+      if (out.count(keyName[idx]))
+	continue;
+
+      auto [it, itOK]        = db.emplace(keyName[idx], std::forward_as_tuple<int, int, int>(-100, -100, -100));
+      const auto [ign, b, c] = it->second;
+      const auto d           = parseTime(keyTime[idx]);
+
+      if ((d - b) <= 60) {
+	out.emplace(it->first);
+      }
+
+      it->second = std::tie(b, c, d);
+    }
+
+    std::vector<std::string> ans(std::make_move_iterator(out.begin()), std::make_move_iterator(out.end()));
+    std::sort(ans.begin(), ans.end());
+
+    return ans;
+  }
+};
+
+int
+main()
+{
+  return 0;
+}
