30d6e216ba8893a9ec35c4fbda8741ddb8f116ea
Teodor Spæren <teodor@sparen.no>
2024-05-26 13:06:05 +0200

    Daily leetcode

---
+++ leetcode/l312.cpp
@@ -0,0 +1,54 @@
+#include <array>
+#include <vector>
+
+class Solution
+{
+
+public:
+  static int maxCoins(const std::vector<int>& nums)
+  {
+    // The key here is to consider what would happen if this was the last
+    // ballon we popped. If that was the case, then the two problems to the
+    // left and right of us would essentially become independent problems.
+    //
+    // We keep l and r to keep track of the limit of the current problem. If
+    // we go outside the bounds, then we will use 1.
+    //
+    // We do a bottom up apporach here, by considering each length in
+    // increasing order.
+
+    const int N = nums.size();
+    const int NN = N + 1;
+
+    std::array<int, 302 * 302> dp;
+
+    // we only need to initialize these, no other for now.
+    for (int i = 0; i < NN; i++)
+      dp[i * NN + i] = 0;
+
+    for (int len = 1; len <= N; ++len) {
+      // Now then we are going to do each of these.
+      for (int left = 0; left <= N - len; left++) {
+        const int right = left + len;
+        const int ll = (left == 0) ? 1 : nums[left - 1];
+        const int rr = (right == N) ? 1 : nums[right];
+
+        const int ourMult = ll * rr;
+
+        int ans = 0;
+        for (int x = left; x < right; x++) {
+          ans = std::max(ans, ourMult * nums[x] + dp[left * NN + x] + dp[(x + 1) * NN + right]);
+        }
+
+        dp[left * NN + right] = ans;
+      }
+    }
+    return dp[0 * NN + N];
+  }
+};
+
+int
+main()
+{
+  return 0;
+}
---
+++ leetcode/l552.cpp
@@ -0,0 +1,99 @@
+#include <array>
+#include <cstdint>
+#include <numeric>
+
+class Solution
+{
+  // To prevent overflow issues, and to minimize number of MODN operations
+  using T = std::int64_t;
+
+  // Matix type
+  using AT = std::array<T, 7 * 7>;
+
+  // row vector type
+  using BT = std::array<T, 7>;
+
+  static constexpr T MODN = 1000000007;
+
+  static constexpr void matrixMult(const AT& a, const AT& b, AT& out)
+  {
+    for (int row = 0; row < 7; row++) {
+      for (int col = 0; col < 7; col++) {
+        out[row * 7 + col] = 0;
+        for (int i = 0; i < 7; i++) {
+          out[row * 7 + col] += (a[row * 7 + i] * b[i * 7 + col]) % MODN;
+        }
+        out[row * 7 + col] %= MODN;
+      }
+    }
+  }
+
+  static constexpr void matrixVecMult(const AT& a, const BT& b, BT& out)
+  {
+    for (int row = 0; row < 7; row++) {
+      out[row] = 0;
+      for (int i = 0; i < 7; i++) {
+        out[row] += (a[row * 7 + i] * b[i]) % MODN;
+      }
+      out[row] %= MODN;
+    }
+  }
+
+public:
+  static constexpr int checkRecord(int n)
+  {
+    // clang-format off
+        // Nx = Not absent yet, last letter was x
+        // Ax = Absent before , last letter was x
+        // Cols: NP, NL, NLL, AP, AL, ALL, AA
+        AT mat = {
+            1, 1, 1, 0, 0, 0, 0,
+            1, 0, 0, 0, 0, 0, 0,
+            0, 1, 0, 0, 0, 0, 0,
+            0, 0, 0, 1, 1, 1, 1,
+            0, 0, 0, 1, 0, 0, 1,
+            0, 0, 0, 0, 1, 0, 0,
+            1, 1, 1, 0, 0, 0, 0
+        };
+
+
+        AT raised = {
+            1, 0, 0, 0, 0, 0, 0,
+            0, 1, 0, 0, 0, 0, 0,
+            0, 0, 1, 0, 0, 0, 0,
+            0, 0, 0, 1, 0, 0, 0,
+            0, 0, 0, 0, 1, 0, 0,
+            0, 0, 0, 0, 0, 1, 0,
+            0, 0, 0, 0, 0, 0, 1,
+        };
+    // clang-format on
+
+    AT tmp;
+
+    // Ok, now we are going to do exponantiation by squaring here.
+    --n;
+    while (0 < n) {
+      if (n & 1) {
+        matrixMult(mat, raised, tmp);
+        raised = tmp;
+      }
+
+      matrixMult(mat, mat, tmp);
+      mat = tmp;
+
+      n >>= 1;
+    }
+
+    BT init = { 1, 1, 0, 0, 0, 0, 1 };
+    BT ans;
+    matrixVecMult(raised, init, ans);
+
+    return std::reduce(ans.begin(), ans.end(), static_cast<T>(0)) % MODN;
+  }
+};
+
+int
+main()
+{
+  return 0;
+}
