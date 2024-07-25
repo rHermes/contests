393403c2e37cfeef8f344835b26b11672948b671
Teodor Spæren <teodor@sparen.no>
2024-07-25 18:00:20 +0200

    Improving leetcode 912

--- leetcode/l912.cpp
+++ leetcode/l912.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <ranges>
 #include <vector>
+
 inline const auto optimize = []() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
@@ -11,50 +12,110 @@ inline const auto optimize = []() {
 
 class Solution
 {
-  template<std::ranges::random_access_range R>
-  static constexpr auto getPivot(R&& r)
-  {
-    const auto a = r.front();
-    const auto b = r[r.size() / 2];
-    const auto c = *(r.end() - 2);
 
-    if (a < b) {
-      if (b < c) {
-        return b;
-      } else if (c < a) {
-        return a;
+  template<std::ranges::random_access_range R>
+  static constexpr void heapSort(R&& r)
+  {
+
+    constexpr auto iLeftChild = [](const int i) { return 2 * i + 1; };
+
+    const int count = std::ranges::size(r);
+    int start = count / 2;
+    int end = count;
+
+    while (1 < end) {
+      if (0 < start) {
+        start--;
       } else {
-        return c;
+        end--;
+        std::swap(r[0], r[end]);
       }
-    } else {
-      if (c < b) {
-        return b;
-      } else if (c < a) {
-        return a;
-      } else {
-        return c;
+
+      int root = start;
+      while (iLeftChild(root) < end) {
+        int child = iLeftChild(root);
+        if (child + 1 < end && r[child] < r[child + 1]) {
+          child++;
+        }
+
+        if (r[root] < r[child]) {
+          std::swap(r[root], r[child]);
+          root = child;
+        } else {
+          break;
+        }
       }
     }
   }
 
   template<std::ranges::random_access_range R>
-  static constexpr void sortIt(R&& r)
+  static constexpr auto getPivot(R&& r)
+  {
+    const auto ai = r.begin();
+    const auto bi = r.begin() + (r.size() / 2);
+    const auto ci = r.end() - 1;
+
+    const auto a = *ai;
+    const auto b = *bi;
+    const auto c = *ci;
+
+    if (a < b) {
+      if (b < c) {
+        return bi;
+      } else if (c < a) {
+        return ai;
+      } else {
+        return ci;
+      }
+    } else {
+      if (c < b) {
+        return bi;
+      } else if (c < a) {
+        return ai;
+      } else {
+        return ci;
+      }
+    }
+  }
+
+  template<std::ranges::random_access_range R>
+  static constexpr void sortIt(R&& r, const int maxDepth)
   {
     // We need to
     const auto sz = std::ranges::size(r);
-    if (sz <= 1) {
+    if (sz <= 16) {
+      for (int i = 1; i < static_cast<int>(sz); i++) {
+        if (r[i - 1] <= r[i]) {
+          continue;
+        }
+        const int val = r[i];
+
+        int j = i - 1;
+        while (0 < j && val < r[j - 1]) {
+          // std::swap(r[j], r[j-1]);
+          j--;
+        }
+
+        std::copy_backward(r.begin() + j, r.begin() + i, r.begin() + i + 1);
+        r[j] = val;
+      }
       return;
-    }
-    if (sz == 2) {
-      if (r[1] < r[0])
-        std::swap(r[0], r[1]);
+    } else if (maxDepth == 0) {
+      // We have gotten a degen case of quicksort, so we abort this
+      // attempt and instead switch over to heap sort.
+      heapSort(r);
       return;
     }
 
-    // pivot element is going to be the midway.
-    const auto val = getPivot(r);
     const auto beg = std::ranges::begin(r);
     const auto end = std::ranges::end(r);
+
+    // pivot element is going to be the midway.
+    const auto pivot = getPivot(r);
+    const auto val = *pivot;
+
+    // We swap the pivot to the front, to avoid non terminating searches.
+    std::swap(*beg, *pivot);
 
     auto left = beg;
     auto right = end - 1;
@@ -79,14 +140,15 @@ class Solution
     const auto leftRange = std::ranges::subrange(beg, pi);
     const auto rightRange = std::ranges::subrange(pi, end);
 
-    sortIt(leftRange);
-    sortIt(rightRange);
+    sortIt(leftRange, maxDepth - 1);
+    sortIt(rightRange, maxDepth - 1);
   }
 
 public:
   static constexpr std::vector<int> sortArray(std::vector<int>& nums)
   {
-    sortIt(nums);
+    const int maxDepth = (std::bit_width(nums.size()) - 1) * 2;
+    sortIt(nums, maxDepth);
     return std::move(nums);
   }
 };
