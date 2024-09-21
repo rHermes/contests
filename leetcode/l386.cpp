34454b85abb5d269930456d3eddec69aafc67090
Teodor Spæren <teodor@sparen.no>
2024-09-21 08:36:01 +0200

    Optimize l386

--- leetcode/l386.cpp
+++ leetcode/l386.cpp
@@ -10,30 +10,26 @@ inline const auto optimize = []() {
 
 class Solution
 {
-  static void fullRange(std::vector<int>& out, const int LIMIT, const int prefix)
-  {
-    for (int i = 0; i < 10; i++) {
-      const int cur = prefix * 10 + i;
-      if (LIMIT < cur)
-        return;
-
-      out.push_back(cur);
-      fullRange(out, LIMIT, cur);
-    }
-  }
-
 public:
-  static std::vector<int> lexicalOrder(const int n)
+  static std::vector<int> lexicalOrder(const int N)
   {
-    std::vector<int> out;
-    out.reserve(n);
+    std::vector<int> out(N);
 
-    for (int i = 1; i <= 9; i++) {
-      if (n < i)
-        break;
+    int cur = 1;
+    for (int idx = 0; idx < N; idx++) {
+      out[idx] = cur;
 
-      out.emplace_back(i);
-      fullRange(out, n, i);
+      // if multiplying the current number by 10 is within the limit, we do so.
+      if (cur * 10 <= N)
+        cur *= 10;
+      else {
+        // We have to adjust the number up a digit instead.
+        while (cur % 10 == 9 || N <= cur) {
+          cur /= 10;
+        }
+
+        cur++;
+      }
     }
 
     return out;
