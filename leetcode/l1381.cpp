cb785726af5f534fde1a4ecf05628d10d60496ae
Teodor Spæren <teodor@sparen.no>
2024-10-01 11:06:28 +0200

    More leetcode

--- leetcode/l1381.cpp
+++ leetcode/l1381.cpp
@@ -1,50 +1,52 @@
-#include <cstdint>
 #include <iostream>
-#include <vector>
+#include <memory>
 
-class CustomStack
-{
-  std::size_t cap_;
-  std::vector<int> data_;
-
-public:
-  CustomStack(int maxSize) : cap_(static_cast<std::size_t>(maxSize)) { data_.reserve(cap_); }
-
-  void push(int x)
-  {
-    if (data_.size() < cap_)
-      data_.emplace_back(x);
-  }
-
-  int pop()
-  {
-    if (data_.empty())
-      return -1;
-
-    auto a = data_.back();
-    data_.pop_back();
-    return a;
-  }
-
-  void increment(int k, int val)
-  {
-    auto level = std::min(static_cast<std::size_t>(k), data_.size());
-    for (std::size_t i = 0; i < level; i++)
-      data_[i] += val;
-  }
-};
-
-/**
- * Your CustomStack object will be instantiated and called as such:
- * CustomStack* obj = new CustomStack(maxSize);
- * obj->push(x);
- * int param_2 = obj->pop();
- * obj->increment(k,val);
- */
-
-static const auto optimize = []() {
+inline const auto optimize = []() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
   std::cout.tie(nullptr);
   return 0;
 }();
+
+class CustomStack
+{
+  std::size_t m_cap;
+  std::size_t m_tail{ 0 };
+  std::unique_ptr<int[]> m_data{ std::make_unique_for_overwrite<int[]>(m_cap) };
+  std::unique_ptr<int[]> m_inc{ std::make_unique<int[]>(m_cap) };
+
+public:
+  constexpr CustomStack(int maxSize) : m_cap(maxSize) {}
+
+  constexpr void push(int x)
+  {
+    if (m_tail != m_cap) {
+      m_data[m_tail++] = x;
+    }
+  }
+
+  constexpr int pop()
+  {
+    if (m_tail == 0)
+      return -1;
+
+    m_tail--;
+
+    const auto ret = m_data[m_tail] + m_inc[m_tail];
+    if (m_tail != 0) {
+      m_inc[m_tail - 1] += m_inc[m_tail];
+    }
+
+    m_inc[m_tail] = 0;
+
+    return ret;
+  }
+
+  constexpr void increment(int k, int val)
+  {
+    auto level = std::min(static_cast<std::size_t>(k), m_tail);
+    if (level != 0) {
+      m_inc[level - 1] += val;
+    }
+  }
+};
---
+++ leetcode/l1497.cpp
@@ -0,0 +1,40 @@
+#include <alloca.h>
+#include <iostream>
+#include <vector>
+
+inline const auto optimize = []() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  return 0;
+}();
+
+class Solution
+{
+public:
+  static bool canArrange(const std::vector<int>& arr, const int K)
+  {
+    // just because I can, I will use a static array here.
+    auto freqs = static_cast<int*>(alloca(sizeof(int) * K));
+    std::ranges::fill_n(freqs, K, 0);
+
+    for (const auto x : arr) {
+      // this is faster than the general solution, as it avoids a second
+      // divisor. On most hardware there will be no branch here
+      int idx = x % K;
+      if (idx < 0)
+        idx += K;
+
+      freqs[idx]++;
+    }
+
+    if (freqs[0] % 2 != 0)
+      return false;
+
+    for (int i = 1; i <= K / 2; i++) {
+      if (freqs[i] != freqs[K - i])
+        return false;
+    }
+    return true;
+  }
+};
